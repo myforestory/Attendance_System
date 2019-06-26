b1ce28d063b760e31cff17141d36b5685b62022e
myforestory <myforestory@hotmail.com.tw>
2019-06-26 19:41:15 +0900

    update 0.5

--- app/src/main/java/com/example/myapplication/UpdateActivity.java
+++ app/src/main/java/com/example/myapplication/UpdateActivity.java
@@ -56,8 +56,8 @@ public class UpdateActivity extends AppCompatActivity {
 
     static int hour, min;
 
-    TextView txtdate, txttime;
-    TextView btntimepicker, btndatepicker;
+    TextView txtdate, txttime, txttime2;
+    TextView btntimepicker, btndatepicker, btntimepicker2;
     Button ggg;
 
     java.sql.Time timeValue;
@@ -229,59 +229,61 @@ public class UpdateActivity extends AppCompatActivity {
         month = c.get(Calendar.MONTH);
         day = c.get(Calendar.DAY_OF_MONTH);
 
-        txtdate = (TextView) findViewById(R.id.etEndTime_update);
+        //txtdate = (TextView) findViewById(R.id.etEndTime_update);
         txttime = (TextView) findViewById(R.id.etStrTime_update);
+        txttime2 = (TextView) findViewById(R.id.etEndTime_update);
 
-        btndatepicker =(Button)findViewById(R.id.ggg);
+//        btndatepicker =(Button)findViewById(R.id.ggg);
         btntimepicker =(TextView) findViewById(R.id.etStrTime_update);
+        btntimepicker2 =(TextView) findViewById(R.id.etEndTime_update);
 
-        btndatepicker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(UpdateActivity.this, "YYYYYY", Toast.LENGTH_SHORT).show();
-                // Get Current Date
-
-                DatePickerDialog dd = new DatePickerDialog(UpdateActivity.this,
-                        new DatePickerDialog.OnDateSetListener() {
-
-                            @Override
-                            public void onDateSet(DatePicker view, int year,
-                                                  int monthOfYear, int dayOfMonth) {
-
-                                try {
-                                    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-                                    String dateInString = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
-                                    Date date = formatter.parse(dateInString);
-
-                                    txtdate.setText(formatter.format(date).toString());
-
-                                    formatter = new SimpleDateFormat("dd/MMM/yyyy");
-
-                                    txtdate.setText(txtdate.getText().toString()+"\n"+formatter.format(date).toString());
-
-                                    formatter = new SimpleDateFormat("dd-MM-yyyy");
-
-                                    txtdate.setText(txtdate.getText().toString()+"\n"+formatter.format(date).toString());
-
-                                    formatter = new SimpleDateFormat("dd.MMM.yyyy");
-
-                                    txtdate.setText(txtdate.getText().toString()+"\n"+formatter.format(date).toString());
-
-                                } catch (Exception ex) {
-
-                                }
-
-
-                            }
-                        }, year, month, day);
-                dd.show();
-            }
-        });
-
+//        btndatepicker.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(UpdateActivity.this, "YYYYYY", Toast.LENGTH_SHORT).show();
+//                // Get Current Date
+//
+//                DatePickerDialog dd = new DatePickerDialog(UpdateActivity.this,
+//                        new DatePickerDialog.OnDateSetListener() {
+//
+//                            @Override
+//                            public void onDateSet(DatePicker view, int year,
+//                                                  int monthOfYear, int dayOfMonth) {
+//
+//                                try {
+//                                    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+//                                    String dateInString = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+//                                    Date date = formatter.parse(dateInString);
+//
+//                                    txtdate.setText(formatter.format(date).toString());
+//
+//                                    formatter = new SimpleDateFormat("dd/MMM/yyyy");
+//
+//                                    txtdate.setText(txtdate.getText().toString()+"\n"+formatter.format(date).toString());
+//
+//                                    formatter = new SimpleDateFormat("dd-MM-yyyy");
+//
+//                                    txtdate.setText(txtdate.getText().toString()+"\n"+formatter.format(date).toString());
+//
+//                                    formatter = new SimpleDateFormat("dd.MMM.yyyy");
+//
+//                                    txtdate.setText(txtdate.getText().toString()+"\n"+formatter.format(date).toString());
+//
+//                                } catch (Exception ex) {
+//
+//                                }
+//
+//
+//                            }
+//                        }, year, month, day);
+//                dd.show();
+//            }
+//        });
         btntimepicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TimePickerDialog td = new TimePickerDialog(UpdateActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_NoActionBar,
                         new TimePickerDialog.OnTimeSetListener() {
                             @Override
                             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -295,6 +297,34 @@ public class UpdateActivity extends AppCompatActivity {
                                     txttime.setText(amPm + "\n" + String.valueOf(timeValue));
                                 } catch (Exception ex) {
                                     txttime.setText(ex.getMessage().toString());
+                                }
+                            }
+                        },
+                        hour, min,
+                        DateFormat.is24HourFormat(UpdateActivity.this)
+                );
+                td.show();
+            }
+        });
+
+        btntimepicker2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog td = new TimePickerDialog(UpdateActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_NoActionBar,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                try {
+                                    String dtStart = String.valueOf(hourOfDay) + ":" + String.valueOf(minute);
+                                    format = new SimpleDateFormat("HH:mm");
+
+                                    timeValue = new java.sql.Time(format.parse(dtStart).getTime());
+                                    txttime2.setText(String.valueOf(timeValue));
+                                    String amPm = hourOfDay % 12 + ":" + minute + " " + ((hourOfDay >= 12) ? "PM" : "AM");
+                                    txttime2.setText(amPm + "\n" + String.valueOf(timeValue));
+                                } catch (Exception ex) {
+                                    txttime2.setText(ex.getMessage().toString());
                                 }
                             }
                         },
